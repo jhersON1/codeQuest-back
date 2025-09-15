c0a914494495c56756305243ce3a2cea4057aca3
Fernando <frr4212@gmail.com>
2025-09-14 21:00:30 -0400

    fix(seed): SeedUsers password now encrypt correctly

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,9 +1,9 @@
-import { HttpCode, Injectable, Logger } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { ObjectLiteral, Repository } from 'typeorm';
 import { User } from '../auth/entities/user.entity';
 import { seedUsers } from './data/seed-users';
-
+import * as argon2 from 'argon2'
 @Injectable()
 export class SeedService {
   private readonly logger = new Logger(SeedService.name);
@@ -11,9 +11,8 @@ export class SeedService {
   constructor(
     @InjectRepository(User)
     private readonly userRepository: Repository<User>,
-  ) {}
+  ) { }
 
-  @HttpCode(200)
   async executeSeed() {
     this.logger.log('Intializing seed process...');
     await this.seedUsers();
@@ -25,7 +24,14 @@ export class SeedService {
   async seedUsers() {
     await this.deleteTableRegisters(this.userRepository);
 
-    const stagedSeedUsers: User[] = this.userRepository.create(seedUsers);
+    const hashedUsers = await Promise.all(seedUsers.map(async user => {
+
+      user.password = await argon2.hash(user.password)
+
+      return user
+    }))
+
+    const stagedSeedUsers: User[] = this.userRepository.create(hashedUsers);
 
     await this.userRepository.save(stagedSeedUsers);
     this.logger.log(`Created ${stagedSeedUsers.length} Users`);
